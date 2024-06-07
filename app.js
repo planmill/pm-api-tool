98c6ea7602f9ad3da9f16594d69c738c23198665
Tinbit Belay <tinbit.belay@planmill.com>
2024-06-07 13:48:57 +0300

    Handle X-Planmill-Auth header for api call, set ajax api calls

--- app.js
+++ app.js
@@ -4,24 +4,69 @@ const session = require('express-session');
 const passport = require('passport');
 const OAuth2Strategy = require('passport-oauth2');
 const cors = require('cors'); 
-const { createProxyMiddleware } = require('http-proxy-middleware');																   
+const { createProxyMiddleware } = require('http-proxy-middleware');	
 const app = express();
 const port = 3000;
 
 // Use the cors middleware
-app.use(cors({
-    origin: 'https://pm-dev01.planmill.com',
-    methods: ['GET','POST','PUT','DELETE','OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+ app.use(cors({
+    origin: '*',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Origin, Content-Type, Authorization, X-Planmill-Auth,Accept,X-Requested-With, UserId, Nonce, Timestamp, Signature',
+    //credentials: true
 }));
 
-//Middleware to hanlde OPTIONS requests
-app.options('*', cors());
+// API endpoint with CORS enabled
+app.get('/api/1.5/products', async (req, res) => {
+    // Get X-Planmill-Auth header
+     const authHeader = req.header('X-Planmill-Auth');
+
+    //Parse the header (Validate custom headers)
+    const headers = authHeader.split(';');
+    let userId, nonce, timestamp, signature;
+
+    for (let i = 0; i < headers.length; i++) {
+    const [key, value] = headers[i].split(':');
+    if (key === 'user') userId = value;
+    else if (key === 'nonce') nonce = value;
+    else if (key === 'timestamp') timestamp = value;
+    else if (key === 'signature') signature = value;
+    }
+   
+    // Check if headers are present
+    if (!userId || !nonce || !timestamp || !signature) {
+      return res.status(400).json({ error: 'Missing authentication headers.' });
+    }
+   
+    //use a secure authentication mechanism (Verify signature (dummy validation)
+    if (signature !== 'Ob3E/6dsRj+ii320sIZFyXGi4f0tqJ3LirI+MUNw3w8=') {
+      return res.status(401).json({ error: 'Invalid signature.' });
+    }
+   
+    try {
+        const response = await fetch('https://online.planmill.com/demo/api/1.5/products');
+        const data = await response.json();
+        res.json(data);
+    } catch (error){
+        console.error('Error fetching data', error);
+        res.status(500).json({error : 'Failed to fetch data'})
+    }
+   
+   });
+   
+
+app.options('*', (req,res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization, X-Planmill-Auth,Accept,X-Requested-With,UserId, Nonce, Timestamp, Signature');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.sendStatus(204);
+});
 
 //Middleware setup
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(session({ secret: 'API-Key', resave: false, saveUninitialized: false }));
+app.use(session({ secret: 'API-ACCESS-KEY', resave: false, saveUninitialized: false }));
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -46,60 +91,60 @@ app.get('/', (req, res) => {
  if (req.session.clientID && req.session.clientSecret && req.session.authorizationURL && req.session.tokenURL && req.session.callbackURL) {
     res.redirect('/login');
  } else {
- res.send(`
- <!DOCTYPE html>
- <html>
- <head>
- <meta name="viewport" content="width=device-width, initial-scale=1">
- <style>
-    .card {
-        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-        transition: 0.3s;
-        width: 80%;
-        align-content: center;
-        padding: 70px;
-    }
-    .card:hover {
-    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-    }
-</style>
- </head>
- <body style="text-align:center; margin-top:100px; padding 70px;padding: 0 0 70px 100px;">
- <div class="card">
-  <h1 style="background-color:#1976D2 !important; color:#fff !important; margin:20px 0 40px 0px; height:76px; text-align:center; padding-top:30px; font-size:3rem;"">Enter PlanMill's OAuth2.0 Credentials</h1>
-    <form action="/set-credentials" method="post" style="height: 500px;">
-        <label for="clientID">Client ID:</label>
-        <input type="text" id="clientID" name="clientID" style="width:500px;height: 24px;" required>
-        <br><br>
-        <label for="clientSecret">Client Secret:</label>
-        <input type="text" id="clientSecret" name="clientSecret" style="width:500px;height: 24px;" required>
-        <br><br>
-        <label for="authorizationURL">Authorization URL:</label>
-        <input type="text" id="authorizationURL" name="authorizationURL" style="width:500px;height: 24px;" required>
-        <br><br>
-        <label for="tokenURL">Token URL:</label>
-        <input type="text" id="tokenURL" name="tokenURL" style="width:500px;height: 24px;" required>
-        <br><br>
-        <label for="callbackURL">CallbackURL:</label>
-        <input type="text" id="callbackURL" name="callbackURL" style="width:500px;height: 24px;" required>
-        <br> <br>
-        <button type="submit" style="background-color:green;width: 100px;height: 30px;border-color: green;color: white;">Submit</button>
-    </form>
-    </div>
-    </body>
- </html>
+    res.send(`
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style>
+            .card {
+                box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+                transition: 0.3s;
+                width: 80%;
+                align-content: center;
+                padding: 70px;
+            }
+            .card:hover {
+            box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+            }
+        </style>
+        </head>
+        <body style="text-align:center; margin-top:100px; padding 70px;padding: 0 0 70px 100px;">
+        <div class="card">
+        <h1 style="background-color:#1976D2 !important; color:#fff !important; margin:20px 0 40px 0px; height:76px; text-align:center; padding-top:30px; font-size:3rem;"">Enter PlanMill's OAuth2.0 Credentials</h1>
+            <form action="/set-credentials" method="post" style="height: 500px;">
+                <label for="clientID">Client ID:</label>
+                <input type="text" id="clientID" name="clientID" style="width:500px;height: 24px;" required>
+                <br><br>
+                <label for="clientSecret">Client Secret:</label>
+                <input type="text" id="clientSecret" name="clientSecret" style="width:500px;height: 24px;" required>
+                <br><br>
+                <label for="authorizationURL">Authorization URL:</label>
+                <input type="text" id="authorizationURL" name="authorizationURL" style="width:500px;height: 24px;" required>
+                <br><br>
+                <label for="tokenURL">Token URL:</label>
+                <input type="text" id="tokenURL" name="tokenURL" style="width:500px;height: 24px;" required>
+                <br><br>
+                <label for="callbackURL">CallbackURL:</label>
+                <input type="text" id="callbackURL" name="callbackURL" style="width:500px;height: 24px;" required>
+                <br> <br>
+                <button type="submit" style="background-color:green;width: 100px;height: 30px;border-color: green;color: white;">Submit</button>
+            </form>
+            </div>
+            </body>
+        </html>
  `);
  }
 });
 
-// Handle form submission to set OAuth credentials
+// Handle form submission to set OAuth2.0 credentials
 app.post('/set-credentials', (req, res) => {
- req.session.clientID = req.body.clientID;
- req.session.clientSecret = req.body.clientSecret;
- req.session.authorizationURL = req.body.authorizationURL;
- req.session.tokenURL = req.body.tokenURL;
- req.session.callbackURL = req.body.callbackURL;
- res.redirect('/login');
+    req.session.clientID = req.body.clientID;
+    req.session.clientSecret = req.body.clientSecret;
+    req.session.authorizationURL = req.body.authorizationURL;
+    req.session.tokenURL = req.body.tokenURL;
+    req.session.callbackURL = req.body.callbackURL;
+    res.redirect('/login');
 });
 
 // Serve the form HTML
@@ -134,51 +179,50 @@ app.get('/auth/callback',
 //Serve the main api demonstation page after authentication
 app.get('/form', ensureAuthenticated, (req, res) => {
  res.send(`
-  <!DOCTYPE html>
+    <!DOCTYPE html>
     <html>
-        <head>
-        <style>
-        .card {
-            box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-            transition: 0.3s;
-            width: 80%;
-            align-content: center;
-            padding: 70px;
-        }
-        .card:hover {
-        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-        }
-       </style>
-        <script>
-            async function submitForm(event) {
-            event.preventDefault();
+    <head>
+    <title>jQuery AJAX Form Example</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        $(document).ready(function() {
+        $('#myForm').on('submit', function(event) {
+            event.preventDefault(); 
+
+            // Generate authentication headers
+            const userId = '50';
+            const nonce = Math.random().toString(36).substr(2, 10); // Random nonce
+            const timestamp = Date.now(); // Current timestamp
+            const signature = 'Ob3E/6dsRj+ii320sIZFyXGi4f0tqJ3LirI+MUNw3w8='; 
+            //const authHeader = "user:" + userId + ";nonce:" + nonce + ";timestamp:" + timestamp + ";signature:" + signature
+            const authHeader = "user:" + 50 + ";nonce:" + 'lqNlcgq' + ";timestamp:" + 1717749973 + ";signature:" + 'Ob3E/6dsRj+ii320sIZFyXGi4f0tqJ3LirI+MUNw3w8='
             const name = document.getElementById('name').value;
-
-            const xhr = new XMLHttpRequest();
-            xhr.open('GET', 'https://pm-dev01.planmill.com/1094120_APIclient_OpenSource_New_Authorization/api/1.5/' + encodeURIComponent(name), true);
-            xhr.setRequestHeader('Content-Type', 'application/json');
-
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState === XMLHttpRequest.DONE) {
-                    if (xhr.status >= 200 && xhr.status < 300) {
-                        const jsonResponse = JSON.parse(xhr.responseText);
-                        document.getElementById('jsonOutput').textContent = JSON.stringify(jsonResponse, null, 2);
-                    } else {
-                        document.getElementById('jsonOutput').textContent = 'Error: ' + xhr.status;
-                    }
+            
+            // Make AJAX request with custom headers
+            $.ajax({
+                url: 'http://localhost:3000/api/1.5/' + encodeURIComponent(name),
+                method: 'GET',
+                dataType: 'json',
+                headers: {
+                'X-Planmill-Auth' : authHeader
+                },
+                success: function(data) {
+                $('#jsonOutput').text(JSON.stringify(data, null, 2));
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                console.error('AJAX error:', textStatus, errorThrown);
                 }
-            };
-
-            xhr.send();
-            }
-        </script>
-        </head>
-        <body style="text-align:center; margin-top:100px; padding 70px;padding: 0 0 70px 100px;">
+            });
+            });
+        });
+    </script>
+    </head>
+    <body style="text-align:center; margin-top:100px; padding 70px;padding: 0 0 70px 100px;">
         <div class="card">
             <h1>PlanMill API testing platform</h1>
             <p>Welcome! You are authenticated.</p>
             <br> <br> <p>This tool help you to test and modify APIs</p>
-            <form onsubmit="submitForm(event)" style="display: flex;">
+            <form onsubmit="submitForm(event)" style="display: flex;" id="myForm">
                 <label for="name">PM API endpoint:</label>
                 <input type="text" id="name" name="name" style="width: 80%; height: 24px;" required>
                 &nbsp;
@@ -204,16 +248,16 @@ app.get('/logout', (req, res) => {
 
 // API endpoint to handle GET request
 app.use('/api/1.5', createProxyMiddleware({
-    target: 'https://pm-dev01.planmill.com/1094120_APIclient_OpenSource_New_Authorization',
+    target: 'http://localhost:3000/',
     changeOrigin: true,
     pathRewrite: {
      '^/api/1.5': '/api/1.5'
     },
     onProxyReq: (proxyReq, req, res) => {
-    proxyReq.setHeader('Access-Control-Allow-Origin', 'https://pm-dev01.planmill.com');
+    proxyReq.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/');
     },
     onProxyRes: (proxyRes, req, res) => {
-    res.header('Access-Control-Allow-Origin', 'https://pm-dev01.planmill.com');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000/');
     }
 }));
 	
